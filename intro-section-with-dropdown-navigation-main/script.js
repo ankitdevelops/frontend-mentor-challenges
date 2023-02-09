414e8890209307fd9034c7f462c18693e674d555
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-02-09 22:55:07 +0530

    Intro section with dropdown navigation complete

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -1,22 +1,63 @@
-const featureDropdown = document.querySelector(".feature__parent");
-featureDropdown.addEventListener("mouseover", () => {
-  document.querySelector(".feature__dropdown").classList.remove("hide");
-});
+const navHumbuggerOpen = document.querySelector(".nav__humbugger");
+const navHumbuggerClose = document.querySelector(".nav__close__humbugger");
+const mobileNav = document.querySelector(".nav__mobile");
+const featureButton = document.querySelector(".feature__parent");
+const companyButton = document.querySelector(".company__parent");
+const mobileFeatureButton = document.querySelector(".mobile__feature__parent");
+const mobileCompanyButton = document.querySelector(".mobile__company__parent");
 
-document
-  .querySelector(".feature__dropdown")
-  .addEventListener("mouseleave", () => {
-    document.querySelector(".feature__dropdown").classList.add("hide");
-  });
-
-const companyDropdown = document.querySelector(".company__parent");
-
-companyDropdown.addEventListener("mouseover", () => {
-  document.querySelector(".company__dropdown").classList.remove("hide");
-});
-
-document
-  .querySelector(".company__dropdown")
-  .addEventListener("mouseleave", () => {
+featureButton.addEventListener("click", () => {
+  if (document.querySelector(".feature__dropdown").classList.contains("hide")) {
     document.querySelector(".company__dropdown").classList.add("hide");
-  });
+    document.querySelector(".feature__dropdown").classList.remove("hide");
+  } else {
+    document.querySelector(".feature__dropdown").classList.add("hide");
+  }
+});
+mobileFeatureButton.addEventListener("click", () => {
+  if (
+    document
+      .querySelector(".mobile__feature__dropdown")
+      .classList.contains("hide")
+  ) {
+    document.querySelector(".mobile__company__dropdown").classList.add("hide");
+    document
+      .querySelector(".mobile__feature__dropdown")
+      .classList.remove("hide");
+  } else {
+    document.querySelector(".mobile__feature__dropdown").classList.add("hide");
+  }
+});
+
+companyButton.addEventListener("click", () => {
+  if (document.querySelector(".company__dropdown").classList.contains("hide")) {
+    document.querySelector(".company__dropdown").classList.remove("hide");
+    document.querySelector(".feature__dropdown").classList.add("hide");
+  } else {
+    document.querySelector(".company__dropdown").classList.add("hide");
+  }
+});
+
+mobileCompanyButton.addEventListener("click", () => {
+  if (
+    document
+      .querySelector(".mobile__company__dropdown")
+      .classList.contains("hide")
+  ) {
+    document
+      .querySelector(".mobile__company__dropdown")
+      .classList.remove("hide");
+    document.querySelector(".mobile__feature__dropdown").classList.add("hide");
+  } else {
+    document.querySelector(".mobile__company__dropdown").classList.add("hide");
+  }
+});
+navHumbuggerOpen.addEventListener("click", () => {
+  mobileNav.classList.remove("hide");
+  document.body.style.backgroundColor = "#00000083";
+});
+
+navHumbuggerClose.addEventListener("click", () => {
+  mobileNav.classList.add("hide");
+  document.body.style.backgroundColor = "hsl(0, 0%, 98%)";
+});
